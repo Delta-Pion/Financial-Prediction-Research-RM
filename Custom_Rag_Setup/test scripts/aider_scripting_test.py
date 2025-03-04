606c0280812107947209cf42d810cb2f547d8f68
Delta-Pion <adi.28per@gmail.com>
2025-03-04 21:34:35 +0530

    Created Bat file for testing aider scripting

--- Custom_Rag_Setup/test scripts/aider_scripting_test.py
+++ Custom_Rag_Setup/test scripts/aider_scripting_test.py
@@ -1,21 +1,24 @@
-# THIS DOES NOT WORK IT REQUIRES PYTHON 12
-
+# Use this C:\Users\Aditya\AppData\Roaming\uv\tools\aider-chat\Scripts\python.exe 
+from dotenv import load_dotenv
 from aider.coders import Coder
 from aider.models import Model
 
+load_dotenv()
 # This is a list of files to add to the chat
 fnames = ["greeting.py"]
 
 model = Model("openrouter/google/gemini-2.0-flash-001")
 
 # Create a coder object
-coder = Coder.create(main_model=model, fnames=fnames)
+coder = Coder.create(main_model=model)
 
 # This will execute one instruction on those files and then return
-coder.run("make a script that prints hello world")
+# coder.run("make a script that prints hello world")
 
-# Send another instruction
-coder.run("make it say goodbye")
+# # Send another instruction
+# coder.run("make it say goodbye")
+
+coder.run("hello wassup")
 
 # You can run in-chat "/" commands too
 coder.run("/tokens")
---
+++ Custom_Rag_Setup/test scripts/aider_test_script.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Users\Aditya\AppData\Roaming\uv\tools\aider-chat\Scripts\python.exe  "%~dp0aider_scripting_test.py" %*
