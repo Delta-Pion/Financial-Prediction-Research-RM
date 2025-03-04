1ce4e3c536e050b14d2e97c50c10e2d066176a1b
Delta-Pion <adi.28per@gmail.com>
2025-03-04 23:04:53 +0530

    Updated script file to call rag_aider and then aider with context

--- Custom_Rag_Setup/raider.bat
+++ Custom_Rag_Setup/raider.bat
@@ -1,2 +1,4 @@
 @echo off
-C:\Users\Aditya\AppData\Roaming\uv\tools\aider-chat\Scripts\python.exe  "%~dp0rag_aider.py" %*
+python "%~dp0rag_aider.py" %*
+C:\Users\Aditya\AppData\Roaming\uv\tools\aider-chat\Scripts\python.exe "%~dp0rag_aider_output.py" %*
+
